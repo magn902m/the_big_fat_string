bd5ed82f091b8d41cbeec92572ce3725281758a8
Magnus <magn902m@stud.kea.dk>
2022-02-07 19:01:45 +0100

    the program is working but only with one opition

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 `use strict`;
 
+const inputText = document.querySelector("#input_text");
+const operator = document.querySelector("#operator");
+const outputText = document.querySelector("#output_text");
+const generateBtn = document.querySelector("#generate");
+
+let pickedOption;
+let input;
+let output;
+let outputResult;
+
 window.addEventListener("DOMContentLoaded", start);
 
-function start() {}
+function start() {
+  console.log("start");
+  generateBtn.addEventListener("click", generate);
+}
+
+function generate() {
+  console.log("generate");
+
+  input = inputText.value;
+  pickedOption = operator.value;
+  console.log(input);
+
+  if (pickedOption === `0`) {
+    outputText.value = input.charAt(0).toUpperCase() + input.substring(1).toLowerCase();
+  } else if (pickedOption === `1`) {
+    console.log(input);
+  } else if (pickedOption === `2`) {
+    console.log(input);
+  } else if (pickedOption === `3`) {
+    console.log(input);
+  } else if (pickedOption === `4`) {
+    console.log(input);
+  } else if (pickedOption === `5`) {
+    console.log(input);
+  } else if (pickedOption === `6`) {
+    console.log(input);
+  } else if (pickedOption === `7`) {
+    console.log(input);
+  }
+}
