8ae77fa17730fc3f83ed90b7ec106dfc44b061d6
Magnus <magn902m@stud.kea.dk>
2022-02-07 23:10:46 +0100

    all are done except for number 8

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const generateBtn = document.querySelector("#generate");
 let pickedOption;
 let input;
 let output;
-let outputResult;
+// let outputResult;
 
 window.addEventListener("DOMContentLoaded", start);
 
@@ -20,25 +20,42 @@ function start() {
 function generate() {
   console.log("generate");
 
+  //   pickedOption = "";
+  //   input = "";
+
   input = inputText.value;
+  //   output = outputText.value;
   pickedOption = operator.value;
   console.log(input);
 
   if (pickedOption === `0`) {
     outputText.value = input.charAt(0).toUpperCase() + input.substring(1).toLowerCase();
   } else if (pickedOption === `1`) {
-    console.log(input);
+    outputText.value = input.substring(input.indexOf(0), input.indexOf(" "));
   } else if (pickedOption === `2`) {
-    console.log(input);
+    outputText.value =
+      input.substring(input.indexOf(0), input.indexOf(" ")).length + ` letters long`;
   } else if (pickedOption === `3`) {
-    console.log(input);
+    outputText.value =
+      `Start index: ` +
+      (input.indexOf(" ") + 1) +
+      `, end index: ` +
+      (input.lastIndexOf(" ") - 1) +
+      `, middle name: ` +
+      input.substring(input.indexOf(" ") + 1, input.lastIndexOf(" "));
   } else if (pickedOption === `4`) {
-    console.log(input);
+    if (input.endsWith(".jpg")) {
+      outputText.value = input.endsWith(".jpg") + `, is a jpg`;
+    } else if (input.endsWith(".png")) {
+      outputText.value = input.endsWith(".png") + `, is a png`;
+    } else {
+      outputText.value = `Is not a jpg or png`;
+    }
   } else if (pickedOption === `5`) {
-    console.log(input);
+    outputText.value = `*`.repeat(input.length);
   } else if (pickedOption === `6`) {
-    console.log(input);
+    outputText.value = input.substring(0, 2) + input.charAt(2).toUpperCase() + input.substring(3);
   } else if (pickedOption === `7`) {
-    console.log(input);
+    outputText.value = input.split(input);
   }
 }
